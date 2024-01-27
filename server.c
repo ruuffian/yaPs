6e8d03c74edf356f7a725a1eb6ddc2679235f3be
ruuffian <ruuffia@gmail.com>
2024-01-27 02:05:55 -0500

    Added port cmdline argument to client and server

--- server.c
+++ server.c
@@ -1,30 +1,45 @@
 #include "include/server.h"
 
-
 #define MAX_QUEUE_SIZE 15
-#define MAX_MSG_SIZE 500
 
-int main(void) {
-  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if(socket_fd == -1){
+int main(int argc, char** argv) {
+  int main_fd, port;
+
+  switch(argc) {
+    case 1:
+      port = DEFAULT_PORT;
+      break;
+    case 2:
+      port = atoi(argv[1]);
+      if(port == 0) {
+        printf("[port] must be between 1024-9999\n");
+        return 1;
+      }
+    default:
+      printf("Usage: server [port]\n");
+      break;
+  }
+
+  main_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if(main_fd == -1){
     printf("[SERVER] [ERROR] Failed to open host socket\n");
     return 1;
   }
   
-  printf("[SERVER] [INFO] Host socket successfully opened on fd [%d]\n", socket_fd);
+  printf("[SERVER] [INFO] Host socket successfully opened on fd [%d]\n", main_fd);
 
   struct sockaddr_in addr;
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(3000);
+  addr.sin_port = htons(DEFAULT_PORT);
   addr.sin_addr.s_addr = INADDR_ANY;
 
-  if(bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1){
+  if(bind(main_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1){
     printf("[SERVER] [ERROR] Failed to bind host socket\n");
     return 1;
   }
 
-  while(listen(socket_fd, MAX_QUEUE_SIZE) == 0) { 
-    int client_fd = accept(socket_fd, 0, 0);
+  while(listen(main_fd, MAX_QUEUE_SIZE) == 0) { 
+    int client_fd = accept(main_fd, 0, 0);
     if(client_fd == -1){
       printf("[SERVER] [NOTICE] Failed to accept client connection\n");
       continue;
@@ -52,7 +67,7 @@ int main(void) {
     } while(msg_size > 0);
 
     close(client_fd);
-    printf("[SERVER] [INFO] Connection on fd [%d] closed\n", client_fd);
+    printf("\n[SERVER] [INFO] Connection on fd [%d] closed\n", client_fd);
   }
   
   return 0;
